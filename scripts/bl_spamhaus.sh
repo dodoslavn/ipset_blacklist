4703709f8b8e8121d05965dd6733875ad6219256
Dodoslav Novak <34769717+dodoslavn@users.noreply.github.com>
2024-09-10 13:16:39 +0200

    Update bl_spamhaus.sh

--- scripts/bl_spamhaus.sh
+++ scripts/bl_spamhaus.sh
@@ -10,5 +10,32 @@ if ! [ -a "../conf/main.conf" ]
 
 . ../conf/main.conf
 
-SMAPHAUS_URL="https://www.spamhaus.org/drop/drop.txt"
-SPAMHAUS_FILE="/tmp/bl_spamhaus.txt"
+rm -f $SPAMHAUS_TMPFILE 2>/dev/null
+
+if [ -a "$SPAMHAUS_TMPFILE" ]
+  then
+  echo "ERROR: Cannot refresh data!"
+  exit 2
+  fi
+
+if [ -z $( ipset list -n | grep ^"$SPAMHAUS_IPSETNAME"$ ) ]
+  then
+  echo "WARNING: IPset $SPAMHAUS_IPSETNAME doesnt exist, creating new"
+  ipset create $SPAMHAUS_IPSETNAME hash:net
+  fi
+
+if [ -z "$( iptables-save | grep $SPAMHAUS_IPSETNAME )" ]
+  then
+  echo "WARNING: IPset $SPAMHAUS_IPSETNAME is not applied in iptables, applying"
+  iptables -I INPUT 1 -m set --match-set $SPAMHAUS_IPSETNAME src -j $SPAMHAUS_RULE
+  fi
+
+wget $SMAPHAUS_URL -O $SPAMHAUS_TMPFILE
+
+for SUBNET in $( grep ^[0-9] $SPAMHAUS_TMPFILE | cut -d';' -f1 )
+  do
+  echo "INFO: Adding "$SUBNET
+  ipset add $SPAMHAUS_IPSETNAME $SUBNET
+  done
+
+rm -f $SPAMHAUS_TMPFILE
