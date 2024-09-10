cbb5a5204cdf3ce06f89c19952f7b9aa14ecbf10
Dodoslav Novak <34769717+dodoslavn@users.noreply.github.com>
2024-09-10 16:36:49 +0200

    Update main.sh

--- scripts/main.sh
+++ scripts/main.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
 
+cd "$(dirname "$0")"
+
+if [ "$( whoami )" != "root" ]
+  then
+  echo "ERROR: You need to be root!"
+  exit 2
+  fi
+
+if ! [ -a "../conf/main.conf" ]
+  then
+  echo "ERROR: Conf file not found!"
+  exit 1
+  fi
+
+./bl_countries.sh
+./bl_de.sh
+./bl_spamhaus.sh
