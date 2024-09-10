d2349d1df69cbbe152c40dd7f957ee05a085b28b
Dodoslav Novak <34769717+dodoslavn@users.noreply.github.com>
2024-09-10 13:36:57 +0200

    Update bl_spamhaus.sh

--- scripts/bl_spamhaus.sh
+++ scripts/bl_spamhaus.sh
@@ -2,6 +2,12 @@
 
 cd "$(dirname "$0")"
 
+if [ "$( whoamo )" != "root" ]
+  then
+  echo "ERROR: You need to be root!"
+  exit 2
+  fi
+
 if ! [ -a "../conf/main.conf" ]
   then
   echo "ERROR: Conf file not found!"
