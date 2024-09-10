70bf83a73514afb61c215daaf63d728e71909960
Dodoslav Novak <34769717+dodoslavn@users.noreply.github.com>
2024-09-10 13:46:36 +0200

    Update bl_countries.sh

--- scripts/bl_countries.sh
+++ scripts/bl_countries.sh
@@ -1,8 +1,36 @@
 #!/bin/bash
 
+cd "$(dirname "$0")"
 
+if [ "$( whoamo )" != "root" ]
+  then
+  echo "ERROR: You need to be root!"
+  exit 2
+  fi
 
+if ! [ -a "../conf/main.conf" ]
+  then
+  echo "ERROR: Conf file not found!"
+  exit 1
+  fi
 
+. ../conf/main.conf
+
+rm -f $COUNTRY_FILE".txt" 2>/dev/null
+
+if [ -a "$COUNTRY_FILE.txt" ]
+  then
+  echo "ERROR: Cannot refresh data!"
+  exit 2
+  fi
+
+wget $COUNTRY_URL -O $COUNTRY_FILE".txt"
+
+COUNTRY_COMPLETELIST="$( grep "aggregated.zone" $COUNTRY_FILE".txt" | cut -d'"' -f2 | cut -d'-' -f1 )"
+
+echo "$COUNTRY_COMPLETELIST"
+
+exit
 
 COUNTRY_URL="http://www.ipdeny.com/ipblocks/data/aggregated/ru-aggregated.zone"
 COUNTRY_FILE="/tmp/bl_country"
