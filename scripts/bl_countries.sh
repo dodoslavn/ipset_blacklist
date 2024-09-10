09dd93b7c9b9cb4a0d167ada1ae1f77118005f1e
Dodoslav Novak <34769717+dodoslavn@users.noreply.github.com>
2024-09-10 13:42:41 +0200

    Update bl_countries.sh

--- scripts/bl_countries.sh
+++ scripts/bl_countries.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+
+
+
+
 
 COUNTRY_URL="http://www.ipdeny.com/ipblocks/data/aggregated/ru-aggregated.zone"
 COUNTRY_FILE="/tmp/bl_country"
+COUNTRY_RULE="DROP"
+COUNTRY_IPSETNAME="blacklist_country_"
+COUNTRY_BLOCK="ru cn"
