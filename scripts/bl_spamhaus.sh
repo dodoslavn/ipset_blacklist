51efea7d72e25fa64ee89edacc60ecf90c143b43
Dodoslav Novak <34769717+dodoslavn@users.noreply.github.com>
2024-09-10 13:05:42 +0200

    Update bl_spamhaus.sh

--- scripts/bl_spamhaus.sh
+++ scripts/bl_spamhaus.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
 
+cd "$(dirname "$0")"
+
+if ! [ -a "../conf/main.conf" ]
+  then
+  echo "ERROR: Conf file not found!"
+  exit 1
+  fi
+
+. ../conf/main.conf
+
+SMAPHAUS_URL="https://www.spamhaus.org/drop/drop.txt"
+SPAMHAUS_FILE="/tmp/bl_spamhaus.txt"
