37a5d9cbbeda7a770ad4a28be242b0a2be2f30e0
dodoslavn <34769717+dodoslavn@users.noreply.github.com>
2026-09-04 12:52:18 +0200

    Refactor refresh.sh to use loop for script execution
    Replaced hardcoded script calls with a loop to execute scripts from a list.

--- scripts/refresh.sh
+++ scripts/refresh.sh
@@ -18,8 +18,14 @@ if ! [ -a "../conf/main.conf" ]
   exit 1
   fi
 
-./bl_countries.sh
-./bl_de.sh
-./bl_spamhaus.sh
+for S in $LIST
+  do
+  echo '###########'
+  echo '###' $S
+  ../sources/"$S".sh
+  done
+#./bl_countries.sh
+#./bl_de.sh
+#./bl_spamhaus.sh
 
 echo "##################"
